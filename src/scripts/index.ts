6b750a1f14330c17443c26187ea66bb5a739ca68
Abhishek Singh <singh4758@gmail.com>
2023-12-08 10:36:33 +0530

    script completion done

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -4,13 +4,14 @@ import {IScript} from "./IScript";
 import ItemsService from "../services/items/items.service";
 
 class Scripts implements IScript {
-    private pageNo: number = 0;
+    private pageNo: number = 1;
 
     public async start(): Promise<void> {
         try {
             const url: string = `${configs.apiUrl}?page=${this.pageNo}&limit=${configs.batches}`;
             const { data: { results } } = await axios.get(url);
-            await ItemsService.bulkInsertUser(results);
+            const data = await ItemsService.bulkInsertUser(results);
+            console.log(`Batch of page ${this.pageNo}. No of records ${data.length} inserted`);
             this.pageNo++;
         } catch (e) {
             console.log("Error found", e)
